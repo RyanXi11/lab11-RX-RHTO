0235ff9137ee047c2a2a3bd8d64bd12f75ee257a
RyanXi11 <ryanxi57@gmail.com>
2025-11-15 12:56:21 -0500

    test_calcualtor changes

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(0, 10,), 0)
+        self.assertEqual(mul(10, 10,), 100)
+        self.assertEqual(mul(1, 5), 5)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertRaises(ZeroDivisionError, div, 0, 10)
+        self.assertEqual(div(10, 10,), 1)
+        self.assertEqual(div(10, 0), 0)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,22 +37,29 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
-    #     fill in code
+    #     fill in
+        self.assertRaises(ValueError, log, 5, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hyp(5, 12), 13)
+        self.assertEqual(hyp(3, 4, ), 5)
+        self.assertEqual(hyp(6, 8,), 10)
 
-    # def test_sqrt(self): # 3 assertions
+
+
+    def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #    square_root(NUM)
     #     # Test basic function
     #     fill in code
-    ##########################
+        self.assertRaises(ValueError,square_root, -10)
+        self.assertEqual(square_root(4), 2)
+        self.assertEqual(square_root(9), 3)
 
 # Do not touch this
 if __name__ == "__main__":
